30f68fb82770fd4d06896b3e1355865703e39a8a
deveucatur <117214850+deveucatur@users.noreply.github.com>
2023-11-17 07:48:23 -0400

    Update relatorio.py

--- relatorio.py
+++ relatorio.py
@@ -1,3 +1,11 @@
+from PIL import Image
+from datetime import date, timedelta
+
+dataAtual = date.today()
+dataFutura = dataAtual + timedelta(days=3)
+dataFormatada = dataFutura.strftime("%d/%m/%Y")
+
+
 def complexidade_name(number):
     aux = {1: 'Fácil',
            2: 'Médio',
@@ -28,7 +36,7 @@ def dadosRelatorio(entregas_list):
                 <td>{complexidade_name(entreg[9])}</td>
                 <td>R${entreg[10]}</td>
             </tr>"""
-
+        
     return htmlDados
 
 def tabelaRelatorio(dadosAux = dict):
@@ -48,23 +56,139 @@ def tabelaRelatorio(dadosAux = dict):
                     <th>Valor</th>
                 </tr>
                 <div>{dadosTABLE}</div>
-            </table>
-            <hr>"""
+            </table>"""
     
     return htmlTabela
 
-def escopoGeral(nameColab, dadosAux, cpf, empresa):
-    tabela = tabelaRelatorio(dadosAux)
+def escopoGeral(nome_colab, list_valores, txt_temporario, name_arquivo):
+    tabela = tabelaRelatorio(list_valores)
+    icone = Image.open('imagens/icone.png')
     
     sum_total = 0
-    for proj, dd in dadosAux.items():
-       sum_total += sum([x[10] for x in dd])
+    for proj, dd in list_valores.items():
+        sum_total += sum([x[10] for x in dd])
+        cpf = list(set([x[13] for x in dd]))[0]
+        empresa = list(set([x[14] for x in dd]))[0]
 
-    htmlGeral = f"""
-            <hr>
+    htmlGeral = f"""<head>
+            <style>
+                body, h1, h3, p, table{{
+                    margin: 0;
+                    padding: 0;
+                }}
+
+                body{{
+                    font-family: Arial, sans-serif;
+                    margin: 0 20px;
+                    padding: 0;
+                }}
+
+                .header{{
+                    text-align: center;
+                }}
+
+                .header img{{
+                    min-width: 100px;
+                    max-width: 100px;
+                    height: auto;
+                }}
+
+                .inf{{
+                    text-align: center;
+                    margin: 0 20px;
+                }}
+
+                .inf h1{{
+                    font-size: 36px;
+                    width: 100%;
+                }}
+
+                .inf p{{
+                    text-align: left;
+                    font-size: 16px;
+                    margin: 0 5px;
+                }}
+
+                .relatorio{{
+                    margin: 0 20px;
+                    text-align: center;
+                }}
+
+                .projeto{{
+                    background-color: #87c772;
+                    border-radius: 0 0 10px 10px;
+                    width: 100%;
+                    margin: auto;
+                    margin-bottom: 20px;
+                    text-align: center;
+                }}
+
+                table{{
+                    width: 100%;
+                    margin: 0 auto;
+                    border-collapse: collapse;
+                    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+                    font-size: smaller;
+                }}
+
+                table th, table td{{
+                    padding: 12px 15px;
+                    text-align: left;
+                    border-bottom: 1px solid #145000;
+                    text-align: center;
+                }}
+
+                table th{{
+                    background-color: #99cb89;
+                }}
+
+                .total{{
+                    background-color: #87c772;
+                    padding: 5px;
+                    width: 95%;
+                    margin: auto;
+                    margin-bottom: 5px;
+                    text-align: right;
+                    border-radius: 8px;
+                }}
+
+                hr{{
+                    margin: 5px 20px;
+                }}
+
+                .atencao{{
+                    text-align: center;
+                    padding: 20px;
+                    border-radius: 8px;
+                }}
+
+                .atencao p{{
+                    margin: 0;
+                    font-weight: bold;
+                    font-size: 1.2em;
+                }}
+
+                .aviso{{
+                    margin-top: 15px;
+                    background-color: #ffe6e6;
+                    border: 1px solid #ff9999;
+                    padding: 10px;
+                    border-radius: 8px;
+                }}
+
+                .aviso p{{
+                    margin: 0;
+                    color: #cc0000;
+                }}
+            </style>
+        </head>
+        <body>
+            <div class="header">
+                <img src="https://www.fag.edu.br/upload/galeria/551c0ac08125a.jpg" alt="Logo da Eucatur">
+            </div>
             <div class="inf">
                 <h1>Extrato de Prestação de Serviço</h1>
-                <p>Nome: {nameColab}</p>
+                <p>Nome: {nome_colab}</p>
                 <p>CPF/CNPJ: {cpf}</p>
                 <p>Empresa: {empresa}</p>
             </div>
@@ -72,94 +196,13 @@ def escopoGeral(nameColab, dadosAux, cpf, empresa):
             <div class="relatorio">{tabela}</div>
             <div class="total">
                 <h3>Valor total: R${sum_total}</h3>
-            </div>"""
+            </div>
+            <div class="atencao">
+                <p>⚠️ Atenção!</p>
+                <div class="aviso">
+                    <p>Emitir nota fiscal até {dataFormatada} e enviar para o DP responsável</p>
+                </div>
+            </div>
+        </body>"""
     
     return htmlGeral
-
-def css_email():
-    relatorioStyle = f"""body, h1, h3, p, table{{
-            margin: 0;
-            padding: 0;
-        }}
-
-        body{{
-            font-family: Arial, sans-serif;
-            margin: 0 20px;
-            padding: 0;
-        }}
-
-        .header{{
-            text-align: center;
-            padding: 20px;
-        }}
-
-        .header img{{
-            max-width: 200px;
-            height: auto;
-        }}
-
-        .inf{{
-            text-align: center;
-            margin: 20px;
-        }}
-
-        .inf h1{{
-            font-size: 36px;
-            width: 100%;
-        }}
-
-        .inf p{{
-            text-align: left;
-            font-size: 16px;
-            margin: 0 5px;
-        }}
-
-        .relatorio{{
-            margin: 0 20px;
-            text-align: center;
-        }}
-
-        .projeto{{
-            background-color: #87c772;
-            border-radius: 0 0 10px 10px;
-            width: 100%;
-            margin: auto;
-            margin-bottom: 20px;
-            text-align: center;
-        }}
-
-        table{{
-            width: 100%;
-            margin: 0 auto;
-            border-collapse: collapse;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            font-size: smaller;
-        }}
-
-        table th, table td{{
-            padding: 12px 15px;
-            text-align: left;
-            border-bottom: 1px solid #145000;
-            text-align: center;
-        }}
-
-        table th{{
-            background-color: #99cb89;
-        }}
-
-        .total{{
-            background-color: #87c772;
-            padding: 5px;
-            width: 95%;
-            margin: auto;
-            margin-bottom: 20px;
-            text-align: right;
-            border-radius: 8px;
-        }}"""
-    
-    return relatorioStyle
-
-#html = escopoGeral()
-#css = css_email()
-#st.write(f'<div>{html}</div>', unsafe_allow_html=True)
-#st.write(f'<style>{relatorioStyle}</style>', unsafe_allow_html=True)
