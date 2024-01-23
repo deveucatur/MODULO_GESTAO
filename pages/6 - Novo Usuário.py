f331a342d04832ea390348d73965734109357eb9
deveucatur <117214850+deveucatur@users.noreply.github.com>
2024-01-23 14:35:21 -0400

    Update 6 - Novo Usuário.py

--- pages/6 - Novo Usuário.py
+++ pages/6 - Novo Usuário.py
@@ -19,7 +19,7 @@ page_title="9box | New User",
 page_icon=Image.open('imagens/icone.png'),
 layout="centered")
 
-tab1, tab2 = st.tabs(['Novos Usuários', 'Usuários Criados'])
+tab1, tab2, tab3 = st.tabs(['Novos Usuários', 'Usuários Criados', 'Check Usuário'])
 
 with tab1:    
     comando1 = 'SELECT * FROM projeu_users;'
@@ -54,83 +54,91 @@ with tab1:
 
     fonte_Projeto = '''@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Bungee+Inline&family=Koulen&family=Major+Mono+Display&family=Passion+One&family=Sansita+Swashed:wght@500&display=swap');'''
     def CadastroDeUsuarios():
-        font_TITLE('CADASTRO DE NOVOS USUÁRIOS', fonte_Projeto,"'Bebas Neue', sans-serif", 49, 'center')
         
+        with st.form('Forms New User', clear_on_submit=False):
+            font_TITLE('CADASTRO DE NOVOS USUÁRIOS', fonte_Projeto,"'Bebas Neue', sans-serif", 49, 'center')
+            
 
-        colu1, colu2 = st.columns([2, 1.6])
-        col1, col2, col3 = st.columns(3)
-        co1, co2, co3 = st.columns([3, 3, 2])
+            colu1, colu2 = st.columns([2, 1.6])
+            col1, col2, col3 = st.columns(3)
+            co1, co2, co3 = st.columns([3, 3, 2])
 
-        with colu1:
-            nome = st.text_input("Nome")
-        with colu2:
-            cpf = st.text_input("CPF / CNPJ")
+            with colu1:
+                nome = st.text_input("Nome")
+            with colu2:
+                cpf = st.text_input("CPF / CNPJ")
 
-        with col1:
-            unidade = st.selectbox("Unidade de Negócio",  unidadesBD)
-        with col2:
-            empresa = st.selectbox('Empresa', list(set([x[1] for x in empresasBD if x[0] != 0])))
-        with col3:
-            macroproc = st.selectbox('Macroprocesso', macroprocBD)
+            with col1:
+                unidade = st.selectbox("Unidade de Negócio",  unidadesBD)
+            with col2:
+                empresa = st.selectbox('Empresa', list(set([x[1] for x in empresasBD if x[0] != 0])))
+            with col3:
+                macroproc = st.selectbox('Macroprocesso', macroprocBD)
 
-        with co1:
-            matricula = st.text_input('Matricula')
-        with co2:
-            condPagamento = st.selectbox("Condição de Pagamento", condPagamentoBD)
-        with co3:
-            codCartao = st.text_input("Código cartão da empresa")
+            with co1:
+                matricula = st.text_input('Matricula')
+            with co2:
+                condPagamento = st.selectbox("Condição de Pagamento", condPagamentoBD)
+            with co3:
+                codCartao = st.text_input("Código cartão da empresa")
 
-        if str(empresa).strip().upper() == 'PJ':
             email2 = st.text_input('Email para receber notificações')
 
-        st.text(' ')
-        especialidade = st.text_area('Especialidades')
-        st.divider()
-        email = st.text_input("Insira seu e-mail/Login")
-        col1, col2 = st.columns(2)
-        with col1:
-            senhaaux =st.text_input("Insira uma senha", type = "password")
-        with col2:
-            senha = st.text_input("Confirme a senha", type = "password")
-        
-        type_user = st.selectbox('Tipo Usuário', ['Padrão', 'Administrador', 'Governança', 'Liderança'])
-        
-        st.text(' ')
-        submitted = st.button("Cadastrar")
+            st.text(' ')
+            especialidade = st.text_area('Especialidades')
+            st.divider()
+            email = st.text_input("Insira seu e-mail/Login")
+            col1, col2 = st.columns(2)
+            with col1:
+                senhaaux =st.text_input("Insira uma senha", type = "password")
+            with col2:
+                senha = st.text_input("Confirme a senha", type = "password")
+            
+            type_user = st.selectbox('Tipo Usuário', ['Padrão', 'Administrador', 'Governança', 'Liderança'])
 
-        if submitted:
-            if senha != senhaaux:
-                st.toast('Senhas diferentes', icon='❌')
-            else:
-                if str(email).strip().lower() not in [str(x[3]).strip().lower() for x in usersBD]:
-                    columnsBD = '''Matricula, Nome, cpf_cnpj, email, senha, unidade_fgkey, empresa_fgkey, macroprocesso_fgkey, condicao_pagamento_fgkey, especialidade, senha_hash, perfil_proj'''
-                    values = f"""{matricula}, 
-                                '{str(nome).strip()}', 
-                                '{str(cpf).strip()}',
-                                '{str(email).strip()}', 
-                                '{senha}',
-                                '{list(set(x[3] for x in dadosPagingBD if x[0] == unidade))[0]}',
-                                '{empresasBD[list(x[1] for x in empresasBD).index(empresa)][0]}',
-                                '{list(set(x[4] for x in dadosPagingBD if x[1] == macroproc))[0]}',
-                                '{list(set(x[5] for x in dadosPagingBD if x[2] == condPagamento))[0]}',
-                                '{str(especialidade).strip()}',
-                                '{stauth.Hasher([senha]).generate()[0]}',
-                                '{func_users(str(type_user).strip())}' """
+            st.text(' ')
+            submitted_new_user = st.form_submit_button("Cadastrar")
 
-                    if str(empresa).strip().upper() == 'PJ':
-                        columnsBD += ', email_pj'
-                        values += f", '{email2}'"
-
-                    senha = str(senha).strip()
-                    mycursor = conexao.cursor()
-                    cmdINSERT = f"""INSERT INTO projeu_users ({columnsBD})
-                                        VALUES ({values});"""
-                    
-                    mycursor.execute(cmdINSERT)
-                    conexao.commit()
-                    mycursor.close()
-                    st.toast('Cadastro Concluído!', icon='✅') 
+            if submitted_new_user:
+                if senha != senhaaux:
+                    st.toast('Senhas diferentes', icon='❌')
                 else:
-                    st.toast('Muito comum email ou senha utilizada.', icon='❌')
+                    #st.info([len(str(x).strip()) for x in [str(nome).strip(), str(cpf).strip(), str(email).strip(),email2, senha, senhaaux, cpf]])           
+                    if 0 not in [len(str(x).strip()) for x in [str(nome).strip(), str(cpf).strip(), str(email).strip(),email2, senha, senhaaux, cpf]]:
+                        if str(email).strip().lower() not in [str(x[3]).strip().lower() for x in usersBD]:
+                            columnsBD = '''Nome, cpf_cnpj, email, senha, unidade_fgkey, empresa_fgkey, macroprocesso_fgkey, condicao_pagamento_fgkey, especialidade, senha_hash, perfil_proj, email_pj'''
+                            values = f""" 
+                                        '{str(nome).strip()}', 
+                                        '{str(cpf).strip()}',
+                                        '{str(email).strip()}', 
+                                        '{senha}',
+                                        '{list(set(x[3] for x in dadosPagingBD if x[0] == unidade))[0]}',
+                                        '{empresasBD[list(x[1] for x in empresasBD).index(empresa)][0]}',
+                                        '{list(set(x[4] for x in dadosPagingBD if x[1] == macroproc))[0]}',
+                                        '{list(set(x[5] for x in dadosPagingBD if x[2] == condPagamento))[0]}',
+                                        '{str(especialidade).strip()}',
+                                        '{stauth.Hasher([senha]).generate()[0]}',
+                                        '{func_users(str(type_user).strip())}', 
+                                        '{email2}' """
+
+
+                            if str(empresa).strip().upper() != 'PJ':
+                                columnsBD += ', Matricula'
+                                values += f', {matricula}'
+
+                            senha = str(senha).strip()
+                            mycursor = conexao.cursor()
+                            cmdINSERT = f"""INSERT INTO projeu_users ({columnsBD})
+                                                VALUES ({values});"""
+                            
+                            mycursor.execute(cmdINSERT)
+                            conexao.commit()
+                            mycursor.close()
+                            st.toast('Cadastro Concluído!', icon='✅') 
+                        else:
+                            st.toast('Email já utilizado.', icon='❌')
+                    else:
+                        st.toast('Primeiramente, preencha todos os campos corretamente.', icon='❌')
+ 
 
     CadastroDeUsuarios()
