1362c21df0d2a0cd78b4e7291dec6d5a5fc62328
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-01-31 17:20:12 -0400

    Retirando página de Validação de Usuário na página de Novos Usuários

--- pages/6 - Novo Usuário.py
+++ pages/6 - Novo Usuário.py
@@ -28,7 +28,7 @@ menuCss = menuProjeuCss()
 st.write(f'<div>{menuHtml}</div>', unsafe_allow_html=True)
 st.write(f'<style>{menuCss}</style>', unsafe_allow_html=True)
 
-tab1, tab2, tab3 = st.tabs(['Novos Usuários', 'Usuários Criados', 'Validar Usuário'])
+tab1, tab2 = st.tabs(['Novos Usuários', 'Usuários Criados'])
 
 with tab1:    
     comando1 = 'SELECT * FROM projeu_users;'
@@ -169,31 +169,5 @@ with tab1:
  
 
     CadastroDeUsuarios()
-
-with tab3:
-    fonte_Projeto = '''@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Bungee+Inline&family=Koulen&family=Major+Mono+Display&family=Passion+One&family=Sansita+Swashed:wght@500&display=swap');'''
-    font_TITLE('VALIDAR E-MAIL', fonte_Projeto,"'Bebas Neue', sans-serif", 49, 'center')
-
-    col1, col2, col3 = st.columns(3)
-    with col2:
-        codValidacao = st.text_input("Código de Validação", max_chars=5)
-        validar = st.button("VALIDAR")
-
-    home = 0
-
-    if validar:
-        if codValidacao:
-            usuario = [x for x in usersBD if x[14] == f'{codValidacao}']
-            if len(usuario) != 0:
-                update = f"UPDATE projeu_users SET status_user = 'A', codigo_user = NULL WHERE codigo_user = '{codValidacao}'"
-                mycursor.execute(update)
-                conexao.commit()
-                st.toast("Usuário validado com sucesso!", icon='✅')
-                # sleep(2)
-                # st.switch_page("Home.py")
-            else:
-                st.info("Esse código não é válido")
-        else:
-            st.info("Insira o código de validação do seu e-mail")
-
+    
 mycursor.close()
