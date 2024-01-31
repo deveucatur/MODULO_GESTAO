e84d47ca5f2fd4e1f924378c8550d63c23ba171a
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-01-31 09:02:47 -0400

    Adicionando header padrão no Dashboard

--- pages/1 - Dashboard.py
+++ pages/1 - Dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import mysql.connector
 from util import string_to_datetime, displayInd, font_TITLE
-from utilR import statusProjetos, css_9box_home, ninebox_home, nineboxDatasUnidades_home
+from utilR import statusProjetos, css_9box_home, ninebox_home, nineboxDatasUnidades_home, menuProjeuHtml, menuProjeuCss
 from datetime import date, datetime
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
@@ -218,6 +218,11 @@ mycursor.execute(cmd_cadeia)
 cadeiaBD = mycursor.fetchall()
 
 mycursor.close()
+
+html = menuProjeuHtml(" ")
+css = menuProjeuCss()
+st.write(f'<div>{html}</div>', unsafe_allow_html=True)
+st.write(f'<style>{css}</style>', unsafe_allow_html=True)
 
 fonte_Projeto = '''@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Bungee+Inline&family=Koulen&family=Major+Mono+Display&family=Passion+One&family=Sansita+Swashed:wght@500&display=swap');'''
 font_TITLE('DASHBOARD DE PROJETOS', fonte_Projeto,"'Bebas Neue', sans-serif", 49, 'center')
