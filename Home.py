7f89b58e7e2500affc9a2b6054736fadd0de2a44
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-02-01 15:26:56 -0400

    Alterando links de acesso nos botões sa página Home

--- Home.py
+++ Home.py
@@ -274,21 +274,21 @@ elif authentication_status:
     with col1:
         st.write("")
         nomeBotão = "CADASTRO DE PROJETOS"
-        link = "https://9box.eucatur.com.br/Dashboard_Administrativo"
+        link = "https://meusprojetos-mpjj-mg.streamlit.app/Cadastro_de_Projetos"
         image_url=  "https://cdn-icons-png.flaticon.com/128/3803/3803936.png"
         botao1(nomeBotão,link,image_url)
 
     with col2:
         st.write("")
         nomeBotão = "MEUS PROJETOS"
-        link = 'https://9box.eucatur.com.br/Dashboard_Administrativo'
+        link = 'https://meusprojetos-mpjj-mg.streamlit.app/Portfólio'
         image_url=  "https://cdn-icons-png.flaticon.com/128/64/64572.png"
         botao1(nomeBotão,link,image_url)
 
     with col3:    
         st.write("")
         nomeBotão = 'PRÊMIO'
-        link = 'https://9box.eucatur.com.br/PDI'
+        link = 'https://meusprojetos-mpjj-mg.streamlit.app/Gestão_de_Prêmios'
         image_url="https://cdn-icons-png.flaticon.com/128/4798/4798118.png"
         botao1(nomeBotão,link,image_url)
 
