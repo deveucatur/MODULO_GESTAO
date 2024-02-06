2d4eca590e9af8f6ef286f644ebe3fe5e817da28
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-02-06 14:33:23 -0400

    Alterando page title

--- pages/5 - Portfólio.py
+++ pages/5 - Portfólio.py
@@ -12,7 +12,7 @@ import plotly.graph_objects as go
 from conexao import conexaoBD
 
 st.set_page_config(
-    page_title="Gerir Projetos",
+    page_title="Gerir Portfólio",
     layout="wide",
     initial_sidebar_state='collapsed')
 
